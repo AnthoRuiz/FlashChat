756ab9cc3e31ea245bd58d245594119092b2ebeb
Anthony Ruiz <yobakaj@gmail.com>
2018-05-30 21:04:53 -0400

    Initial Commit

---
+++ Flash Chat/CustomMessageCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomMessageCell.swift
+//  Flash Chat
+//
+//  Created by Anthony Ruiz on 5/30/18.
+//  Copyright © 2018 Jakaboy. All rights reserved.
+//
+
+import UIKit
+
+class CustomMessageCell: UITableViewCell {
+
+
+    @IBOutlet var messageBackground: UIView!
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var messageBody: UILabel!
+    @IBOutlet var senderUsername: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code goes here
+        
+        
+        
+    }
+
+
+}
